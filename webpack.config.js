a9fe56423b44196cae3d91597910bf896848425a
gilad1987 <gilad1987@gmail.com>
2016-06-02 14:55:57 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -141,6 +141,13 @@ if (process.env.NODE_ENV === 'development') {
         allChunks: true
       })
   );
+  config.module.loaders[1].loaders.push(
+      ExtractTextPlugin.extract('css!sass')
+  );
+
+}
+if (process.env.NODE_ENV === 'production') {
+
   config.plugins.push(
       new ExtractTextPlugin("app/assets/stylesheets/main.css",{
         allChunks: true
